2dc8021283d048f895453e24b6caaa8c40ddd1d3
Marcello Calvo <mchellocz@gmail.com>
2022-11-07 21:44:48 -0600

    [init] Initial commit

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5.0F;
+
+    [SerializeField]
+    float bodySpeed = 5.0F;
+
+    [SerializeField]
+    GameObject bodyPrefab;
+
+    [SerializeField]
+    int gap = 100;
+
+    Rigidbody rb;
+
+    float xDirection;
+
+    List<GameObject> bodyParts = new List<GameObject>();
+    List<Vector3> positionHistory = new List<Vector3>();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            GrowSnake();
+        }
+    }
+
+    void Update()
+    {
+        xDirection = Input.GetAxisRaw("Horizontal");
+        
+        rb.transform.position += rb.transform.forward * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            rb.transform.Rotate(0, 90 * xDirection, 0);
+        }
+
+        positionHistory.Insert(0, rb.transform.position);
+
+        int index = 0;
+        foreach(var body in bodyParts)
+        {
+            Vector3 point = positionHistory[Mathf.Min(index * gap, positionHistory.Count - 1)];
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+            body.transform.LookAt(point);
+            index++;
+        }
+    }
+    void GrowSnake()
+    {
+        GameObject body = Instantiate(bodyPrefab);
+        bodyParts.Add(body);
+    }
+}
