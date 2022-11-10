e06f1205a4cb0553787107e6ff89e071b631bbb8
Ian Fallas <ian.f1999@gmail.com>
2022-11-10 12:28:50 -0600

    Main Menu with Random level

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
+
+    [SerializeField]
+    int maxSceneIndex = 3;
+
     public Animator animator;
 
     private int levelToLoad;
@@ -30,7 +34,12 @@ public class LevelController : MonoBehaviour
         SceneManager.LoadScene(levelToLoad);
     }
 
-
+    public void FadetoLevelRandom() {
+        
+        levelToLoad = Random.Range(1, maxSceneIndex);
+        Debug.Log(string.Format("Changing level: {0}",levelToLoad));
+        animator.SetTrigger("FadeOut");
+    }
 
     
 }
